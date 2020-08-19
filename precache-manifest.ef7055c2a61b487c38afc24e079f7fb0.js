935643f42d4da0c8379f9b70e0b4fc2c62fa0780
vaibhavkumar1 <vaibhav.kumar_cs18@gla.ac.in>
2020-08-19 11:35:13 +0530

    Updates

---
+++ precache-manifest.ef7055c2a61b487c38afc24e079f7fb0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3d8ce2b0b39193123e3b3e5f587c415",
+    "url": "/http//vaibhavkumar1.github.io/react_app/index.html"
+  },
+  {
+    "revision": "37d4b3ca01a8796bfcf4",
+    "url": "/http//vaibhavkumar1.github.io/react_app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5b594dd8f970e14e633c",
+    "url": "/http//vaibhavkumar1.github.io/react_app/static/js/2.4138a59f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/http//vaibhavkumar1.github.io/react_app/static/js/2.4138a59f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37d4b3ca01a8796bfcf4",
+    "url": "/http//vaibhavkumar1.github.io/react_app/static/js/main.3fd2702c.chunk.js"
+  },
+  {
+    "revision": "f07055558846a4e5cda4",
+    "url": "/http//vaibhavkumar1.github.io/react_app/static/js/runtime-main.da08d3c7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/http//vaibhavkumar1.github.io/react_app/static/media/logo.5d5d9eef.svg"
+  }
+]);
